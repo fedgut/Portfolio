734fc0770d142d4c56090fe343aaab3129dc2281
fedgut <fedgut@gmail.com>
2020-09-10 20:37:42 -0500

    Add Theme, grid and provider from @rmwc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import '@rmwc/theme/styles';
+import '@rmwc/grid/styles';
 import './App.css';
+
+import { Theme, ThemeProvider } from '@rmwc/theme';
+import { Grid, GridRow, GridCell } from '@rmwc/grid';
 
 function App() {
   return (
     <div className="App">
-
+      <ThemeProvider
+        options={{
+          primary: 'white',
+          secondary: 'white',
+          primaryBg: 'black',
+          secondaryBg: '#b49bac',
+        }}
+      >
+        <Grid>
+          <GridRow>
+            <GridCell span={8}>a</GridCell>
+            <GridCell span={4}>b</GridCell>
+          </GridRow>
+        </Grid>
+      </ThemeProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RMWCProvider } from '@rmwc/provider';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RMWCProvider>
+      <App />
+    </RMWCProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
