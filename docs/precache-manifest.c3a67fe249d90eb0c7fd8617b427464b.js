c5c41df5df50705f3c9a4922ca00a5764f9d070c
fedgut <fedgut@gmail.com>
2020-09-18 18:52:29 -0500

    Create /docs for publishing

---
+++ docs/precache-manifest.c3a67fe249d90eb0c7fd8617b427464b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47d21e48a33a160076b6c5508b3a856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f19b99b3b4cc19b1ab",
+    "url": "/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f19b99b3b4cc19b1ab",
+    "url": "/static/js/main.0ce69b9e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
---
+++ docs/static/js/main.0ce69b9e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var o=n(0),r=n.n(o),a=n(2),c=n.n(a);n(8);var i=function(){return r.a.createElement("div",{className:"App"})};c.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(i,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.0ce69b9e.chunk.js.map
