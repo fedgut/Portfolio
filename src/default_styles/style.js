617f6f123d9a24b4fd04f4256940c71b8a1a2ad0
fedgut <fedgut@gmail.com>
2020-09-18 15:02:29 -0500

    refactor to wrap color constants in a function

--- src/default_styles/style.js
+++ src/default_styles/style.js
@@ -1,16 +1,34 @@
 // colors for coral
-const darkGray = '#0c1011';
-const gray = '#939395';
-const lightGray = '#EDEDEE';
-const coral = '#ff524e';
-const coralContrast = '#c33e3c';
+function colarColors() {
+  const darkGray = '#0c1011';
+  const gray = '#939395';
+  const lightGray = '#EDEDEE';
+  const coral = '#ff524e';
+  const coralContrast = '#c33e3c';
+  return {
+    gray, coral, coralContrast, darkGray, lightGray,
+  };
+}
+
+const {
+  gray, coral, coralContrast, darkGray, lightGray,
+} = colarColors();
 
 // colors for aqua
-const aqua = '#0f8d99';
-const blueGray = '#96a9a3';
-const lightBlue = '#afd9dd';
-const black = '#000000';
-const blueGrayContrast = '#688378';
+function aquaColors() {
+  const aqua = '#0f8d99';
+  const blueGray = '#96a9a3';
+  const lightBlue = '#afd9dd';
+  const black = '#000000';
+  const blueGrayContrast = '#688378';
+  return {
+    aqua, blueGray, lightBlue, black, blueGrayContrast,
+  };
+}
+
+const {
+  aqua, blueGray, lightBlue, black, blueGrayContrast,
+} = aquaColors();
 
 // common elements
 const white = '#ffffff';
