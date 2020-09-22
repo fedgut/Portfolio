e3618fa749e487a68fdc275aaba17be23fd5ea2a
fedgut <fedgut@gmail.com>
2020-09-21 20:12:23 -0500

    Add projects internal link

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const paragraph1 =
   ' I am an ever-curious person, always looking to learn, grow, and explore.';
 
 const paragraph2 =
-  'Among my tech-stack is React, Redux, Ruby, Ruby on Rails, JavaScript, and SQL. I embrace code standards and testing as best practices to achieve readable, maintainable code.';
+  'Among my tech-stack is React, Redux, Ruby, Ruby on Rails, JavaScript, and SQL. I embrace code standards and testing as best practices to achieve readable, maintainable code. ';
 
 const paragraph3 =
   "When I'm not coding, I lead a handmade engagement rings brand, Petit Bribon. My experience running a small business has taught me to always deliver on time, with quality, and keeping clients in the loop along the way. ";
@@ -21,6 +21,16 @@ const About = props => {
           <br />
           <br />
           {paragraph2}
+          <a
+            style={{
+              fontSize: 'inherit',
+              color: secondary.colorContrast,
+              backgroundColor: secondary.color,
+            }}
+            href="#projects"
+          >
+            See my projects
+          </a>
         </div>
         <div className="about-section2">
           {paragraph3}
@@ -34,6 +44,7 @@ const About = props => {
           >
             Get my Resume
           </a>
+          <br />
         </div>
       </div>
     </div>
